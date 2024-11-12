a938b195ef2a54f97c5ce31016c44d9b002e3628
darshangoswami <goswamidarshan999@gmail.com>
2024-11-11 19:05:39 -0500

    added Merge Intervals

---
+++ Merge Intervals/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) == 1:
+            return intervals
+
+        intervals.sort(key=lambda x:x[0])
+
+        res = []
+
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+
+        return res
